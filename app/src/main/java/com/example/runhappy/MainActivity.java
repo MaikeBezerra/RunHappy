57a2e543f90b374994be2887d56123808aaf9081
Jonas Deyvid <jonasdeyvid@hotmail.com>
2019-09-25 00:05:47 -0300

    Adicionando tela inicial em partes (1)

--- app/src/main/java/com/example/runhappy/MainActivity.java
+++ app/src/main/java/com/example/runhappy/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.runhappy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_boas_vindas);
+        setContentView(R.layout.activity_tela_inicial);
     }
 }
