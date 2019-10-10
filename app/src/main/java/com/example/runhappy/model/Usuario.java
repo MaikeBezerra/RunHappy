0e23df63ca1374c3922ef159c0b650d4ea97de92
MaikeBezerra <maike_ti@hotmail.com>
2019-10-10 14:17:17 -0300

    Criando model usuario

---
+++ app/src/main/java/com/example/runhappy/model/Usuario.java
@@ -0,0 +1,58 @@
+package com.example.runhappy.model;
+
+import java.util.Date;
+
+public class Usuario {
+
+    private Integer id;
+    private String nome;
+    private String email;
+    private String senha;
+    private Date dataNacimento;
+
+    public Usuario(){}
+
+    public Usuario(String nome, String email, String senha, Date dataNacimento){
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.dataNacimento = dataNacimento;
+    }
+
+    public Usuario(Integer id, String nome, String email, String senha, Date dataNacimento){
+        this(nome, email, senha, dataNacimento);
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Date getDataNacimento() {
+        return dataNacimento;
+    }
+
+    public void setDataNacimento(Date dataNacimento) {
+        this.dataNacimento = dataNacimento;
+    }
+}
