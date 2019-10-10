ec99b431029a7191dd0f4e9369d72b12651d332b
MaikeBezerra <maike_ti@hotmail.com>
2019-10-09 23:08:17 -0300

    modificações simples

--- app/src/main/java/com/example/runhappy/MainActivity.java
+++ app/src/main/java/com/example/runhappy/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_inicial);
+        setContentView(R.layout.activity_boas_vindas);
     }
 }
