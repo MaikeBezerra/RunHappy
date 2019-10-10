25927fa4b29742885a6f5dffe4f26a6dba140705
MaikeBezerra <maike_ti@hotmail.com>
2019-10-10 13:18:06 -0300

    Interação entre tela de boas vindas e tela de cadastro

--- app/src/main/java/com/example/runhappy/MainActivity.java
+++ app/src/main/java/com/example/runhappy/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boas_vindas);
+
+        Button cadastrar = (Button) findViewById(R.id.botao_cadastre_se);
+
+    }
+
+    public void cadastrar(View view){
+        Intent casdastro = new Intent(this, CadastroActivity.class);
+        startActivity(casdastro);
     }
 }
