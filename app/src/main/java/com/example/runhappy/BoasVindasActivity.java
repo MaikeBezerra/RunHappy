fe4bad1e865cba69997aae78983cb62b10441f53
MaikeBezerra <maike_ti@hotmail.com>
2019-10-26 20:58:32 -0300

    Verificação de usuario já logado no sistema

--- app/src/main/java/com/example/runhappy/BoasVindasActivity.java
+++ app/src/main/java/com/example/runhappy/BoasVindasActivity.java
@@ -2,13 +2,24 @@ package com.example.runhappy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class BoasVindasActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boas_vindas);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if (firebaseAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), TelaInicialActivity.class));
+        }
     }
 }
