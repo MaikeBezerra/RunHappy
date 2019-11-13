6f6bc869cf2e3882b32764d6aecfbf1e67d94884
Jonas Deyvid <jonasdeyvid@hotmail.com>
2019-11-13 16:07:50 -0300

    Histórico corrigido

--- app/src/main/java/com/example/runhappy/ConfirmacaoFinalizarCorridaActivity.java
+++ app/src/main/java/com/example/runhappy/ConfirmacaoFinalizarCorridaActivity.java
@@ -5,16 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.runhappy.ui.corrida.CorridaViewModel;
+
 public class ConfirmacaoFinalizarCorridaActivity extends AppCompatActivity {
+
+    private CorridaViewModel model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmacao_finalizar_corrida);
+//        model = new CorridaViewModel(this);
+//        model.setValuesAttributes();
     }
 
     public void confirmar(View view){
-
+//        model.salvar();
         finish();
     }
 }
