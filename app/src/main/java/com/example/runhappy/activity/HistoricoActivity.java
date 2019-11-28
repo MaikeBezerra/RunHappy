87da348f5225603fde077fab0c5aca9aabcc2947
MaikeBezerra <maike_ti@hotmail.com>
2019-11-27 23:51:55 -0300

    Adição presenter onCorridaListEventListener gerar historico de corridas

--- app/src/main/java/com/example/runhappy/activity/HistoricoActivity.java
+++ app/src/main/java/com/example/runhappy/activity/HistoricoActivity.java
@@ -1,39 +1,27 @@
 package com.example.runhappy.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.runhappy.R;
-import com.example.runhappy.data.CorridaDAO;
-import com.example.runhappy.data.SQLite.CorridaDAOSQLite;
-import com.example.runhappy.data.SQLite.SQLiteHandle;
-import com.example.runhappy.data.SQLite.UsuarioDAOSQLite;
-import com.example.runhappy.data.firebase.CorridaDBFirebase;
-import com.example.runhappy.model.Usuario;
-import com.example.runhappy.ui.corrida.CorridaAdapter;
+
+import com.example.runhappy.ui.corrida.CorridaListView;
 
 public class HistoricoActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
-    private CorridaAdapter corridaAdapter;
-    private Usuario usuario;
 
-    private CorridaDAO corridaDAO;
+    private CorridaListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historico);
 
-        recyclerView = findViewById(R.id.rvCorrida);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);
+        listView = new CorridaListView(getApplicationContext());
+        View rootView = findViewById( android.R.id.content );
 
-        corridaDAO = new CorridaDBFirebase(getApplicationContext());
+        listView.inicialize( rootView );
 
-        corridaAdapter = new CorridaAdapter(corridaDAO.getCorridas());
-        recyclerView.setAdapter(corridaAdapter);
     }
 }
---
+++ app/src/main/java/com/example/runhappy/presenter/OnCorridaListEventListener.java
@@ -0,0 +1,10 @@
+package com.example.runhappy.presenter;
+
+import com.example.runhappy.model.Corrida;
+
+import java.util.List;
+
+public interface OnCorridaListEventListener {
+
+    void onSetList(List<Corrida> corridas);
+}
