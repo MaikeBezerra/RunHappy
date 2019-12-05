c0b937429fa7a22ec5c0c5acc5dfd6ec6d30faff
MaikeBezerra <maike_ti@hotmail.com>
2019-12-05 00:01:11 -0300

    Atualização historico

--- app/src/main/java/com/example/runhappy/ui/usuario/UsuarioListView.java
+++ app/src/main/java/com/example/runhappy/ui/usuario/UsuarioListView.java
@@ -2,7 +2,6 @@ package com.example.runhappy.ui.usuario;
 
 import android.content.Context;
 import android.view.View;
-import android.widget.AdapterView;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
